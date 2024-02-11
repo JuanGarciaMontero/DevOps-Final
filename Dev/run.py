79be7bf9b88befd7183dfb1dfe35cb5dc33df339
Juan García Montero <jgmen20@gmail.com>
2024-02-12 00:10:25 +0100

    Cambios en run.py

--- Dev/run.py
+++ Dev/run.py
@@ -7,6 +7,7 @@ env_name = os.getenv("FLASK_ENV", "development")
 app = create_app(env_name)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0")
+
 
 
