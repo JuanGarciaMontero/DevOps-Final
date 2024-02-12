acf2bbbc35bdf95f843370844cddf42ee51fd89a
Juan García Montero <jgmen20@gmail.com>
2024-02-12 12:03:05 +0100

    Mod Jenkins y run.py

--- Dev/run.py
+++ Dev/run.py
@@ -7,7 +7,7 @@ env_name = os.getenv("FLASK_ENV", "development")
 app = create_app(env_name)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=5000)
 
 
 
