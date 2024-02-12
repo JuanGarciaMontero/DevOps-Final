ad39585d89fbb4a8d36fea6ba59db73a86bc535d
Juan García Montero <jgmen20@gmail.com>
2024-02-12 21:46:26 +0100

    Mod jenkins manage.sh

--- QA/manage.sh
+++ QA/manage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Requires the database to be up
-FLASK_DEBUG=1
-export FLASK_DEBUG
+
+export FLASK_DEBUG=1
 export FLASK_ENV=development
 export DATABASE_URI=postgresql://postgres:juan@127.0.0.1:5432/ejer_final
 export SECRET_KEY=secret
