9060fbf2d5d1a70363f86b2eba4b30a6cb62faa3
Juan García Montero <jgmen20@gmail.com>
2024-02-13 18:02:24 +0100

    Cambios en ci.yaml y manage.sh

--- manage.sh
+++ manage.sh
@@ -3,7 +3,7 @@
 # Requires the database to be up
 
 export FLASK_ENV=development
-export DATABASE_URI=postgresql://postgres:juan@127.0.0.1:5432/ejer_final
+export DATABASE_URI=postgresql://postgres:postgres@127.0.0.1:5432/ejer_final
 export SECRET_KEY=secret
 
 python manage.py
