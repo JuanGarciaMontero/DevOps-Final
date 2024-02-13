3f956425477317cb10121f7ca806953666bbcb13
Juan García Montero <jgmen20@gmail.com>
2024-02-13 17:41:04 +0100

    cambios en manage.sh

--- manage.sh
+++ manage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Requires the database to be up
-export FLASK_DEBUG=1
+
 export FLASK_ENV=development
 export DATABASE_URI=postgresql://postgres:juan@127.0.0.1:5432/ejer_final
 export SECRET_KEY=secret
