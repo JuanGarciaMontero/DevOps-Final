d948cf8bafa1b7dc1023b5d2429569f4863cbf3a
Juan García Montero <jgmen20@gmail.com>
2024-02-13 22:38:15 +0100

    Quito Export en manage.sh Dev

--- manage.sh
+++ manage.sh
@@ -2,8 +2,8 @@
 
 # Requires the database to be up
 
-export FLASK_ENV=development
-export DATABASE_URI=postgresql://postgres:postgres@127.0.0.1:5432/ejer_final
-export SECRET_KEY=secret
+FLASK_ENV=development
+DATABASE_URI=postgresql://postgres:postgres@127.0.0.1:5432/ejer_final
+SECRET_KEY=secret
 
 python manage.py
