fafb3684e1b52d49b9db1954e331e6a8fc5eb75e
Juan García Montero <jgmen20@gmail.com>
2024-02-13 20:15:56 +0100

    Cambios en Dev

--- Dev/manage.sh
+++ Dev/manage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Requires the database to be up
-export FLASK_DEBUG=1
+
 export FLASK_ENV=development
-export DATABASE_URI=postgresql://postgres:juan@127.0.0.1:5432/ejer_final
+export DATABASE_URI=postgresql://postgres:postgres@127.0.0.1:5432/ejer_final
 export SECRET_KEY=secret
 
 python manage.py
