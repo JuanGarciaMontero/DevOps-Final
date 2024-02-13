60dd6ce5aa21b462b123726d8b2cf4254aae7b5b
Juan García Montero <jgmen20@gmail.com>
2024-02-13 21:16:38 +0100

    Actions QA

---
+++ manage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Requires the database to be up
+
+FLASK_ENV=development
+DATABASE_URI=postgresql://postgres:postgres@127.0.0.1:5432/ejer_final
+SECRET_KEY=secret
+FLASK_COVERAGE=1
+
+flask test
+python manage.py
