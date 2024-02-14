eae4430bd373928860709d1d081b18732806ed04
JuanGarciaMontero <108418765+JuanGarciaMontero@users.noreply.github.com>
2024-02-14 20:18:43 +0100

    Delete Dev directory
    Quito carpeta Dev de Dev

--- Dev/manage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Requires the database to be up
-
-export FLASK_ENV=development
-export DATABASE_URI=postgresql://postgres:postgres@127.0.0.1:5432/ejer_final
-export SECRET_KEY=secret
-
-python manage.py
