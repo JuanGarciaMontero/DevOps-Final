86fa14c00268922e75e091cfbf2c90667fbbb4db
Juan García Montero <jgmen20@gmail.com>
2024-03-13 23:21:38 +0100

    Cambios en el entorno coverage QA

--- manage.sh
+++ manage.sh
@@ -2,9 +2,9 @@
 
 # Requires the database to be up.
 
-FLASK_ENV=coverage
+FLASK_ENV=development
 DATABASE_URI=postgresql://postgres:postgres@0.0.0.0:5432/ejer_final
 SECRET_KEY=secret
-FLASK_COVERAGE=1
+FLASK_COVERAGE=2
 
 python manage.py
